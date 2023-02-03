9a573b455676fa03b194a10e43879c2e0cca277d
Dario Elguero <darioelguero@gmail.com>
2023-02-03 17:05:03 -0300

    unnecessary removed code

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,4 @@
-import React from "react";
-import '../../styles/styles.sass'
-
 export default function Home() {
-  let numero: number = 10;
-  numero = 22;
   return (
     <div className=''>
       <h1>Este es el home</h1>
