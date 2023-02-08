e868928ba7746b876cc9090bd53e83c6f3fe9e09
NorSAUCE <you@example.com>
2023-02-08 18:44:11 -0300

    trying logo

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-  return
-    <div>Navbar</div>;
-  
+  return <div>
+    <Link to ='/'>
+      <a>
+      <img src="/assets/logo/LogoMobile.png"/>
+      </a>
+    </Link>
+  </div>;
 }
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,9 @@
+import Navbar from "../../components/Navbar/Navbar"
 
 export default function Home() {
   return (
     <div className=''>
+      <Navbar/>
       <h1>Este es el home</h1>
     </div>
   );
