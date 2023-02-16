c111661653874d6eb7a439ea7d9fb5c0fde68ff7
Dario Elguero <73416791+Dario-Elguero@users.noreply.github.com>
2023-02-16 13:27:45 -0300

    removed "navbar" component the home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
-import Navbar from "../../components/Navbar/Navbar";
 import ProductCarousel from "../../components/ProductCarousel/ProductCarousel";
 import InstaCarousel from "../../components/InstaCarousel/InstaCarousel";
 
 export default function Home() {
   return (
     <div className="">
-      <Navbar />
       <ProductCarousel />
       <InstaCarousel />
     </div>
