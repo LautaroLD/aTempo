b52d2e5e39346a6b4513d6f76fd645d3955490d8
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-02-21 11:33:35 -0300

    finished-edit-direction

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,27 +1,45 @@
 import { useState } from "react";
+import EditDirection from "../../components/ProfileComponents/EditDirection/EditDirection";
 import EditPassword from "../../components/ProfileComponents/EditPassword/EditPassword";
 import EditProfile from "../../components/ProfileComponents/EditProfile/EditProfile";
 
 export default function Profile() {
   const [editProfile, setEditProfile] = useState<boolean>(false);
+  const [editDirection,setEditDirection] = useState<boolean>(false);
 
   const [email, setEmail] = useState<string>("jonathan@hotmail.com");
   const [name, setName] = useState<string>("Jonathan");
   const [surname, setSurname] = useState<string>("Poblet");
   const [document, setDocument] = useState<number>(15212525);
   const [birthdate, setBirthdate] = useState<Date>(new Date());
+
+  const [country, setCountry] = useState<string>('Argentina');
+  const [state, setState] = useState<string>('Buenos Aires');
+  const [city, setcity] = useState<string>('Lomas de Zamora');
+  const [street, setStreet] = useState<string>('Hipolito Yrigoyen');
+  const [number, setNumber] = useState<number>(9201);
+  const [zipCode, setZipCode] = useState<number>(1832);
   return (
     <div className="profile">
       <button
         onClick={(): void => {
           setEditProfile(!editProfile);
+          setEditDirection(false);
         }}
         className="profile__buttons"
       >
         Editar perfil
       </button>
       <button className="profile__buttons">Mis compras</button>
-      <button className="profile__buttons">Mi dirección</button>
+      <button
+        onClick={(): void => {
+          setEditProfile(false);
+          setEditDirection(!editDirection);
+        }}
+        className="profile__buttons"
+        >
+          Mi dirección
+      </button>
       <button className="profile__buttons">Mi lista de deseados</button>
       <button className="profile__buttons">Mis valoraciones</button>
       {editProfile && (
@@ -36,6 +54,18 @@ export default function Profile() {
           <EditPassword />
         </>
       )}
+      {
+        editDirection && (
+          <EditDirection
+            country={country}
+            state={state}
+            city={city}
+            street={street}
+            number={number}
+            zipCode={zipCode}
+          />
+        )
+      }
     </div>
   );
 }
