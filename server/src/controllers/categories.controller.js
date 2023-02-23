d76b991fd7126763d8f12c79830ef0692dc8156f
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2023-02-23 14:48:59 -0300

    feat: category routes and controllers
    feat:categories controllers
    
    feat:categories routes

--- server/src/controllers/categories.controller.js
+++ server/src/controllers/categories.controller.js
@@ -1,16 +1,72 @@
-
+const { Category } = require('../database/models/index')
 
 // Show all categories
-const showAllCategories = async (req,res)=> {
-    let categories = await Category.findAll(id);
-    if(categories){
-        return res.status(200).json({categories});
+const showAllCategories = async (req, res) => {
+    let categories = await Category.findAll();
+    if (categories) {
+        return res.status(200).json({ categories });
     } else {
-        return res.status(404).json({'msg': 'no se encontro el artista '});
+        return res.status(404).json({ 'message': 'categories not found' });
+    }
+}
+
+const showCategoryById = async (req, res) => {
+    let { id } = req.params
+    let category = await Category.findByPk(id)
+
+    if (category) {
+        return res.status(200).json({ category })
+    } else {
+        return res.status(404).json({ 'message': 'category not found' })
+    }
+}
+
+const createCategory = async (req, res) => {
+    let params = req.body
+    const category = await Category.create(params)
+
+    if (category) {
+        return res.status(200).json({ 'message': 'category created succesfully', category })
+    } else {
+        return res.status(200).json({ 'message': 'data has not been received' })
+    }
+}
+
+const updateCategory = async (req, res) => {
+    let { id } = req.params
+    const category = await Category.findByPk(id);
+
+    if (category) {
+        const data = req.body
+        category.name = data.name
+
+        category.save().then(cat => {
+            return res.status(200).json({ 'message': 'category updated successfully', 'category': cat })
+        })
+    } else {
+        return res.status(404).json({ 'message': 'category not found' })
+    }
+}
+
+const deleteCategory = async (req, res) => {
+    let { id } = req.params
+    const category = await Category.findByPk(id);
+
+    if (category) {
+        category.destroy()
+            .then(() => {
+                return res.status(200).json({ 'message': 'categoy deleted successfully' })
+            })
+    } else {
+        return res.status(404).json({ 'message': 'category not found' })
     }
 }
 
 // Exports
 module.exports = {
-    showAllCategories
+    showAllCategories,
+    showCategoryById,
+    createCategory,
+    updateCategory,
+    deleteCategory
 }
--- server/src/routes/categoty.routes.js
+++ server/src/routes/categoty.routes.js
@@ -1,8 +1,16 @@
 const Router = require('express');
 const routerCategories = Router();
 
-const { showAllCategories } = require('../controllers/categories.controller');
+const { showAllCategories,
+    showCategoryById,
+    createCategory,
+    updateCategory,
+    deleteCategory } = require('../controllers/categories.controller');
 
 routerCategories.get('/', showAllCategories);
+routerCategories.get('/:id',showCategoryById);
+routerCategories.post('/', createCategory);
+routerCategories.put('/:id', updateCategory);
+routerCategories.delete('/:id', deleteCategory);
 
 module.exports = routerCategories;
